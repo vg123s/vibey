fa7feb3a67f3517f2ab8e82c56480fc7e9c7cb52
Suzy <sulagnaghosh52@gmail.com>
2023-06-30 18:06:43 +0530

    feat: add more fucntionalites

--- server/src/middeware/auth.middleware.js
+++ server/src/middeware/auth.middleware.js
@@ -1,7 +1,15 @@
 import { expressjwt } from 'express-jwt';
+import jwksRsa from 'jwks-rsa';
 
 export const checkJwt = (credentialRequired = true) =>
   expressjwt({
+    secret: jwksRsa.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://www.googleapis.com/oauth2/v3/certs',
+    }),
     algorithms: ['RS256'],
+    issuer: ['https://accounts.google.com'],
     credentialsRequired: credentialRequired,
   });
